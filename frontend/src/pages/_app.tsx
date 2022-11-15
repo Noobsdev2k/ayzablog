cd3c0cde62f7d7b8d961c63fdfaa89c6d95acc08
Noobsdev2k <99718570+Noobsdev2k@users.noreply.github.com>
2022-11-15 12:25:12 +0700

    #4 create file themes.ts
    create file themes.ts success

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,15 +4,26 @@ import '../utils/assets/styles/globals.css';
 import { NextUIProvider } from '@nextui-org/react';
 import { Provider } from 'react-redux';
 import { store } from '@/configs/store/store';
+import { darkTheme, lightTheme } from '@/configs/themes';
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
-            <NextUIProvider>
-                <Layouts>
-                    <Component {...pageProps} />
-                </Layouts>
-            </NextUIProvider>{' '}
+            <ThemeProvider
+                defaultTheme="system"
+                attribute="class"
+                value={{
+                    light: lightTheme.className,
+                    dark: darkTheme.className,
+                }}
+            >
+                <NextUIProvider>
+                    <Layouts>
+                        <Component {...pageProps} />
+                    </Layouts>
+                </NextUIProvider>
+            </ThemeProvider>
         </Provider>
     );
 }
